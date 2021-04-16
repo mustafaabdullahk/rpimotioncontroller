97594cec11d1104ce7c0f5e14aa45c13505ec51d
Mustafa Abdullah Kuş <mustafa.kus@sparsetechnology.com>
2021-04-16 17:08:35 +0300

    Update grpc service name

--- python/main.py
+++ python/main.py
@@ -18,7 +18,7 @@ class Greeter (motioncontroller_pb2_grpc.DeviceService):
 
 def mcserver():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    motioncontroller_pb2_grpc.add_DeviceServiceServicer_to_server(DeviceServer(), server)
+    motioncontroller_pb2_grpc.add_DeviceServiceServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
