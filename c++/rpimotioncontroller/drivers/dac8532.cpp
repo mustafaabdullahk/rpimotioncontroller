15c67bb363ce7fca521d230467ffdc0e83f4187b
Muatafa Abdullah Kuş <mustafa.kus@sparsetechnology.com>
2021-04-22 22:24:35 +0300

    Fix dac out voltage function bug

--- c++/rpimotioncontroller/drivers/dac8532.cpp
+++ c++/rpimotioncontroller/drivers/dac8532.cpp
@@ -8,12 +8,12 @@ dac8532::dac8532(QObject *parent) : QObject(parent)
 void dac8532::configModule(uint8_t channel, uint16_t data)
 {
 	config cfg;
-	cfg.digitalWrite(CS_PIN, 1);
-	cfg.digitalWrite(CS_PIN, 0);
+	cfg.digitalWrite(CS_DAC_PIN, 1);
+	cfg.digitalWrite(CS_DAC_PIN, 0);
 	cfg.spiWriteByte(channel);
 	cfg.spiWriteByte((data >> 8));
 	cfg.spiWriteByte((data & 0xff));
-	cfg.digitalWrite(CS_PIN, 1);
+	cfg.digitalWrite(CS_DAC_PIN, 1);
 }
 
 void dac8532::outVoltage(uint8_t channel, float voltage)
