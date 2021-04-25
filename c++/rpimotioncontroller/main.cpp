601d33e564e966308ad624cdf353c2cc255ec41a
Muatafa Abdullah Kuş <mustafa.kus@sparsetechnology.com>
2021-04-25 13:06:10 +0300

    Update grpc server functions

--- c++/rpimotioncontroller/main.cpp
+++ c++/rpimotioncontroller/main.cpp
@@ -13,8 +13,11 @@
 #include <grpc/grpc.h>
 #include <thread>
 
+#include "drivers/ads1256.h"
 #include "drivers/dac8532.h"
+#include "drivers/gpiodriver.h"
 #include "3rdparty/debug.h"
+#include "drivercontext.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -36,51 +39,62 @@ void runMyServer(motioncontroller::DeviceService::Service *myserv)
 	server->Wait();
 }
 
-class RaspberryServer : public motioncontroller::DeviceService::Service
-{
+class RaspberryServer : public motioncontroller::DeviceService::Service{
+
 public:
-	grpc::Status SayHello(grpc::ServerContext *context, const motioncontroller::HelloRequest *request, motioncontroller::HelloReply *response);
-	grpc::Status GetDigitalValues(grpc::ServerContext *context, const google::protobuf::Empty *request, ::grpc::ServerWriter<motioncontroller::Analog2Digital> *writer);
-	grpc::Status OutVoltage(grpc::ServerContext *context, const motioncontroller::Digital2Analog *request, motioncontroller::CommandResult *response);
-	grpc::Status ConfigureAnalog2Digital(grpc::ServerContext *context, const motioncontroller::Analog2DigitalConfig *request, motioncontroller::CommandResult *response);
-	grpc::Status SetDiffChannel(grpc::ServerContext *context, const motioncontroller::SetDiffChnAnalog2Digital *request, motioncontroller::CommandResult *response);
-};
-
-grpc::Status RaspberryServer::SayHello(grpc::ServerContext *context, const motioncontroller::HelloRequest *request, motioncontroller::HelloReply *response)
-{
-	qDebug()<< "hello";
-	return grpc::Status::OK;
-}
-
-grpc::Status RaspberryServer::GetDigitalValues(grpc::ServerContext *context, const google::protobuf::Empty *request, ::grpc::ServerWriter<motioncontroller::Analog2Digital> *writer)
-{
-}
-
-grpc::Status RaspberryServer::OutVoltage(grpc::ServerContext *context, const motioncontroller::Digital2Analog *request, motioncontroller::CommandResult *response)
-{
+	grpc::Status SayHello(grpc::ServerContext *context, const motioncontroller::HelloRequest *request, motioncontroller::HelloReply *response)
+	{
+	}
+	grpc::Status GetDigitalValues(grpc::ServerContext *context, const google::protobuf::Empty *request, ::grpc::ServerWriter<motioncontroller::Analog2Digital> *writer)
+	{
+	}
+	grpc::Status OutVoltage(grpc::ServerContext *context, const motioncontroller::Digital2Analog *request, motioncontroller::CommandResult *response)
+	{
 	dac8532 dac;
-	dac.outVoltage(channel_A, 3.3);
+	ads1256 adc;
+	dac.outVoltage(channel_A, request->channel_voltage());
 	dac.outVoltage(channel_B, request->channel_voltage());
 	std::cout << "set voltage : " << request->channel_voltage() << std::endl;
 	response->set_command_result("ok");
+	adc.getAll();
+	for (int i = 0; i < 8; i++) {
+		printf("%d %f\r\n", i, adc.adcBuffer[i] * 5.0 / 0x7fffff);
+	}
 	return grpc::Status::OK;
-}
-
-grpc::Status RaspberryServer::ConfigureAnalog2Digital(grpc::ServerContext *context, const motioncontroller::Analog2DigitalConfig *request, motioncontroller::CommandResult *response)
+	}
+	grpc::Status ConfigureAnalog2Digital(grpc::ServerContext *context, const motioncontroller::Analog2DigitalConfig *request, motioncontroller::CommandResult *response)
+	{
+	}
+	grpc::Status SetDiffChannel(grpc::ServerContext *context, const motioncontroller::SetDiffChnAnalog2Digital *request, motioncontroller::CommandResult *response)
+	{
+	}
+};
+/*
+grpc::Status RaspberryServer::OutVoltage(grpc::ServerContext *context, const motioncontroller::Digital2Analog *request, motioncontroller::CommandResult *response)
 {
-}
+//	dac8532 dac;
+	ads1256 adc;
+//	dac.outVoltage(channel_A, request->channel_voltage());
+//	dac.outVoltage(channel_B, request->channel_voltage());
+	std::cout << "set voltage : " << request->channel_voltage() << std::endl;
+	response->set_command_result("ok");
+	adc.getAll();
+	for (int i = 0; i < 8; i++) {
+		printf("%d %f\r\n", i, adc.adcBuffer[i] * 5.0 / 0x7fffff);
+	}
+	return grpc::Status::OK;
+}*/
 
-grpc::Status RaspberryServer::SetDiffChannel(grpc::ServerContext *context, const motioncontroller::SetDiffChnAnalog2Digital *request, motioncontroller::CommandResult *response)
-{
-}
+
 
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 	loguru::g_stderr_verbosity = 3;
-	loguru::init(argc, argv);
+	loguru::init(argc, argv);	
 	gInfoS() << "cilettttt";
+	DriverContext::instance();
 	std::thread threadServer([=]() { runMyServer(new RaspberryServer); });
 	return a.exec();
 }
